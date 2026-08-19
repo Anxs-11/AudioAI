6c2aca294a6985da178e8dd772c84ab654a4d97a
Mohd Anas <mohd.anas@teradata.com>
2026-08-19 20:28:18 +0530

    Make API URL configurable via VITE_API_URL env var for split deployment

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -5,7 +5,9 @@
 
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL || "/api",
+});
 
 // Inject stored JWT into every request
 api.interceptors.request.use((config) => {
